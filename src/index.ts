8f7db628f139653e8e68d84731af763c32b144fb
Mehmet Köse <mehmetkose@Mehmets-MacBook-Air.local>
2019-06-26 10:13:36 +0200

    add healthcheck

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,10 @@ app.get('/divide/:dividend/:divisor', (req, res) => {
     return res.status(200).send((req.params.dividend / req.params.divisor).toLocaleString());
 });
 
+app.get('/pong', (req, res) => {
+    return res.status(200).send('pong');
+});
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`);
 });
