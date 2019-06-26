029887e9499c5f24d1c2b1bde9873049a20dab57
Mauro Mandracchia <mauromandracchia@gmail.com>
2019-06-26 10:49:14 +0200

    Add prometheus

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 import express from 'express';
+import promMid from 'express-prometheus-middleware';
+
 const app = express();
 const port = 3000;
+
+app.use(promMid({
+    metricsPath: '/metrics',
+    collectDefaultMetrics: true,
+    requestDurationBuckets: [0.1, 0.5, 1, 1.5],
+}));
 
 app.get('/divide/:dividend/:divisor', (req, res) => {
     return res.status(200).send((req.params.dividend / req.params.divisor).toLocaleString());
