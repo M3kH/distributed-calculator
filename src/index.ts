01a1c9582a6d4ffbfdfd2913410cdcdcbae48991
Mauro Mandracchia <mauromandracchia@gmail.com>
2019-06-26 10:09:37 +0200

    ✨ Add express server

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+const app = express();
+const port = 3000;
+
+app.get('/divide/:dividend/:divisor', (req, res) => {
+    return res.status(200).send((req.params.dividend / req.params.divisor).toLocaleString());
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`);
+});
